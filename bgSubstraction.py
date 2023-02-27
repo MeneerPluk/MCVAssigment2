1d59ea35126c7300949ea2f07af612ba0ddf5c72
rfigge24 <rubenfigge24@gmail.com>
2023-02-27 16:36:33 +0100

    substraction works now

--- bgSubstraction.py
+++ bgSubstraction.py
@@ -19,7 +19,7 @@ def trainBackgroundModel(bgSubstractor, bgVidPath):
             cv.waitKey(10)
 
 
-def testBackgroundModel(bgSubstractor, fgVidPath):
+def testBackgroundModel(bgSubstractor, fgVidPath, dilation):
     vid = cv.VideoCapture(fgVidPath)
     nrOfFrames = vid.get(cv.CAP_PROP_FRAME_COUNT)
     kernel = np.zeros((5,5))
@@ -31,26 +31,40 @@ def testBackgroundModel(bgSubstractor, fgVidPath):
             blur = cv.GaussianBlur(img,(3,3), 0)
             #substracting the background:
             fgImg = bgSubstractor.apply(blur, None, 0)
+            
             #finding the contour:
-            contours, hierarchy  = cv.findContours(fgImg, cv.RETR_EXTERNAL, cv.CHAIN_APPROX_SIMPLE)
+            contours, hierarchy  = cv.findContours(fgImg, cv.RETR_CCOMP, cv.CHAIN_APPROX_SIMPLE)
+            list = [len(x) for x in contours]
+            indexes = np.argsort(list,0)
+
             testimg = np.zeros_like(fgImg)
 
-            fgImg = cv.drawContours(testimg, contours, (len(contours)-1) , (255,0,0), -1)
+            fgImg = cv.drawContours(testimg, contours, indexes[-1] , (255,255,255), -1)
+
+            kernel = np.ones((3,3),np.uint8) 
+            fgImg = cv.dilate(fgImg,kernel, iterations = dilation)
+
+
 
             cv.imshow('bgImg', fgImg)
-            cv.waitKey(250)
+            cv.waitKey(1)
 
 
 
 
 
-vidpath2 = os.path.abspath('data/cam1/video.avi')
-vidpath = os.path.abspath('data/cam1/background.avi')
+vidpath2 = os.path.abspath('data/cam4/video.avi')
+vidpath = os.path.abspath('data/cam4/background.avi')
 fgbg = cv.createBackgroundSubtractorMOG2(150, 100, True)
 fgbg.setShadowValue(0)
+fgbg.setShadowThreshold(0.4)
 
 
+#cam 1 = 100, 0.5, no dilation
+#cam 2 = 100, 0.42, with 2 iterations of dilation
+#cam 3 = 100, 0.5, no dilation
+#cam 4 = 100, 0.5, no dilation
 
 
 trainBackgroundModel(fgbg,vidpath)
-testBackgroundModel(fgbg, vidpath2)
+testBackgroundModel(fgbg, vidpath2, 0)

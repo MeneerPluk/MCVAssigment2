3ca28d0fbc0766e514e7ac98ac2d4f1eed313b76
rfigge24 <rubenfigge24@gmail.com>
2023-03-02 15:24:40 +0100

    marchingcubes now has a function to call

--- marchingCubes.py
+++ marchingCubes.py
@@ -5,36 +5,38 @@ import voxels as v
 from skimage import measure
 from skimage.draw import ellipsoid
 
-# Generate voxel coords of frame:
-coords = v.initilizeVoxels()
+def showVoxelsAsMesh(voxelGrid):
+    # Use marching cubes to obtain the surface mesh of these ellipsoids
+    verts, faces, normals, values = measure.marching_cubes(voxelGrid, 0)
 
-# turing it into a 3d array one hot encoding:
-voxelGrid = np.zeros((50,50,100))  
-for cord in coords:
-    x,y,z = cord[0],-cord[2],cord[1]
-    voxelGrid[x,y,z] = 1
+    # Display resulting triangular mesh using Matplotlib. This can also be done
+    # with mayavi (see skimage.measure.marching_cubes docstring).
+    fig = plt.figure(figsize=(10, 10))
+    ax = fig.add_subplot(111, projection='3d')
+
+    # Fancy indexing: `verts[faces]` to generate a collection of triangles
+    mesh = Poly3DCollection(verts[faces])
+    mesh.set_edgecolor('k')
+    ax.add_collection3d(mesh)
+
+    ax.set_xlabel("x-axis: 2cm per unit")
+    ax.set_ylabel("y-axis: 2cm per unit")
+    ax.set_zlabel("z-axis: 2cm per unit")
+
+    ax.set_xlim(0, 65)
+    ax.set_ylim(0, 65)
+    ax.set_zlim(0, 65)
+
+    plt.tight_layout()
+    plt.show()
 
 
-# Use marching cubes to obtain the surface mesh of these ellipsoids
-verts, faces, normals, values = measure.marching_cubes(voxelGrid, 0)
+if __name__ == "__main__":
+    # Generate voxel coords of frame:
+    coords = v.initilizeVoxels()
 
-# Display resulting triangular mesh using Matplotlib. This can also be done
-# with mayavi (see skimage.measure.marching_cubes docstring).
-fig = plt.figure(figsize=(10, 10))
-ax = fig.add_subplot(111, projection='3d')
-
-# Fancy indexing: `verts[faces]` to generate a collection of triangles
-mesh = Poly3DCollection(verts[faces])
-mesh.set_edgecolor('k')
-ax.add_collection3d(mesh)
-
-ax.set_xlabel("x-axis: 2cm per unit")
-ax.set_ylabel("y-axis: 2cm per unit")
-ax.set_zlabel("z-axis: 2cm per unit")
-
-ax.set_xlim(0, 65)
-ax.set_ylim(0, 65)
-ax.set_zlim(0, 65)
-
-plt.tight_layout()
-plt.show()
+    # turing it into a 3d array one hot encoding:
+    voxelGrid = np.zeros((50,50,100))  
+    for cord in coords:
+        x,y,z = cord[0],-cord[2],cord[1]
+        voxelGrid[x,y,z] = 1
